9a8deb04ea732e672432b7bdc775a5f9d8fd0331
Felipe Lauksas <flauksas@br.ibm.com>
2023-12-04 20:45:36 -0300

    Adde resource location log

--- src/main/java/com/mux/cnpj/batch/job/reader/MultiResourceItemReaderCNPJ.java
+++ src/main/java/com/mux/cnpj/batch/job/reader/MultiResourceItemReaderCNPJ.java
@@ -1,6 +1,7 @@
 package com.mux.cnpj.batch.job.reader;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.context.annotation.Lazy;
@@ -22,8 +23,10 @@ public class MultiResourceItemReaderCNPJ<T> extends MultiResourceItemReader<T> {
 		log.info("Found {} resources {} to process", FILE_NAME_PATTERN, resources.length);
 		this.setName("multiResourceItemReader");
 		this.setResources(resources);
+		this.setStrict(true);
 		FlatFileReaderCNPJ<T> delegate = new FlatFileReaderCNPJ<T>(clazz, COLUMN_NAMES, INCLUDED_FIELDS);
 		this.setDelegate(delegate);
+		
 	}
 
 	public Resource[] getResources(String resourcePattern) {
@@ -31,8 +34,17 @@ public class MultiResourceItemReaderCNPJ<T> extends MultiResourceItemReader<T> {
 		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 		try {
 			String locationPattern = "file://" + resourcePattern;
+			log.info("locationPattern: {}", locationPattern);
 			resources = resolver
 					.getResources(locationPattern);
+
+			for (int i = 0; i < resources.length; i++) {
+				Resource resource = resources[i];
+				log.info("Resource {} path: {}",
+						i,
+						Paths.get(resource.getURI()).toFile().getAbsolutePath());
+			}
+
 		} catch (IOException e) {
 			log.error("error loading resources", e);
 		}
